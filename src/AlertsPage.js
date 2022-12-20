270e30a7c6b123e9c39f415ac7595c25d0eb009e
cherieliu <cherieliu27@gmail.com>
2022-12-19 21:37:21 -0800

    updated alerts page

--- src/AlertsPage.js
+++ src/AlertsPage.js
@@ -6,7 +6,25 @@ import NavigationMenu from "./Menu";
 export default function AlertsPage() {
     return (
         <div className="smartphone">
-            
+            <div className="menu">
+                <div className="header">
+                    <Fragment>
+                        <br></br>
+                        <strong>ALERTS</strong>
+                    </Fragment>
+                    <div className="switch">
+                        <NavigationMenu />
+                    </div>
+                </div>
+                <div className="new">
+                    <div className="newtext">
+                    <Fragment>
+                        <br></br>
+                        <strong> NEW </strong>
+                    </Fragment>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
